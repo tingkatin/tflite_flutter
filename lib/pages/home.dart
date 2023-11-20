299b1d36e558c5982c62d72cd1cba809e3200fcb
tingkatin <info.tingkatin@gmail.com>
2023-11-20 11:29:11 +0700

    Initial branching

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -29,7 +29,7 @@ class _HomeState extends State<Home> {
   File? imagePreview;
   ImagePicker imagePicker = ImagePicker();
   ImageClassificationHelper? imageClassificationHelper;
-  double threshold = 0.5;
+  double threshold = 0.6;
 
   void _initializeState() {
     imageClassificationHelper = ImageClassificationHelper();
